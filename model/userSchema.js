695ad51350f45db00a927dda7a8034d087315540
Aniket shrivastava <aniket.shrivastava91@gmail.com>
2023-12-29 10:13:53 +0530

    create a userSchema

---
+++ model/userSchema.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose")
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "User name is required"],
+        minLength: [5, "Name must be least 5 char"],
+        maxLength: [15, "Name must be least 15 char"],
+        trim: true
+
+    },
+    email: {
+        type: String,
+        required: [true, "User email is required"],
+        lowercase: true,
+        unique: [true, "Already registered email"],
+
+    },
+    password: {
+        type: String,
+        select: false
+    },
+
+    forgotPasswordToken: {
+        type: String
+    },
+    forgotPasswordExpairyDate: {
+        type: String
+
+    }
+
+})
+
+const userModel = ("user", userSchema)
+module.exports = userSchema
