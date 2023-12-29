042726af220831ab50e3ea3204de7a42b2a9dc90
Aniket shrivastava <aniket.shrivastava91@gmail.com>
2023-12-29 10:33:27 +0530

    add a user_controller

--- authController/authController.js
+++ authController/authController.js
@@ -1,9 +1,31 @@
-const signup =(req,res)=>{
-    const {name ,email,password, comfirmPassword} = req.body
- return res.status(202).json({
-    success: true,
-    data: {}
- })
+const userModel = require("../model/userSchema")
+const signup =async(req,res)=>{
+    const {name ,email,password, comfirmPassword} = req.body;
+   try {
+    const userInfo = userModel(req.body);
+    const result = await userInfo.save();
+
+    return res.status(200).json({
+        success:true,
+        data: result
+    })
+   } catch (error) {
+    if (error.code === 11000) {
+        
+        return res.status(400).json({
+            success:false,
+            message:"Account already exits"
+        })
+    }
+    return res.status(400).json({
+        success:false,
+        message:error.message
+    })
+   }
+
+
+
+ 
 }
 
 module.exports = {signup}
