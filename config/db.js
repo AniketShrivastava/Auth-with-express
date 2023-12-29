06376d1dbef94cbd4931164ea04018ddf0a7fbf9
Aniket shrivastava <aniket.shrivastava91@gmail.com>
2023-12-29 09:57:09 +0530

    mongodb connected

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017"
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb://127.0.0.1:27017"
 
 const databaseconnect = ()=>{
     mongoose.connect(MONGODB_URL).
