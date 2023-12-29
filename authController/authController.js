4b5de1ab91ab5a36494ec48764716dbe60a60fbc
Aniket shrivastava <aniket.shrivastava91@gmail.com>
2023-12-29 10:50:16 +0530

    user controller validation

--- authController/authController.js
+++ authController/authController.js
@@ -1,7 +1,28 @@
 const userModel = require("../model/userSchema")
+const emailValidator = require("email-validator")
 const signup =async(req,res)=>{
     const {name ,email,password, comfirmPassword} = req.body;
    try {
+    if(!name || !email || !password || !comfirmPassword){
+        return res.status(400).json({
+            success:false,
+            message:"Every field is required"
+        })
+    }
+    var validEmail = emailValidator.validate(email)
+    if(!validEmail){
+        return res.status(400).json({
+            success:false,
+            message: "Please provide valid email"
+        })
+    }
+
+    if(password !== comfirmPassword){
+        return res.status(400).json({
+            success:false,
+            message:"Password & confirm password don't match"
+        })
+    }
     const userInfo = userModel(req.body);
     const result = await userInfo.save();
 
