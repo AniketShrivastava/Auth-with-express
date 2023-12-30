4b7d3544b2340a9b4d209fed127d4ca0bb538d93
Aniket shrivastava <aniket.shrivastava91@gmail.com>
2023-12-30 17:05:42 +0530

    add bcrypt

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const authRouter = require("./routes/authRoute");
 const databaseconnect = require("./config/db");
+const cookieParser = require("cookie-parser")
 
 
 require("dotenv").config();
@@ -8,6 +9,8 @@ require("dotenv").config();
 const app = express();
 
 databaseconnect();
+app.use(cookieParser());
+
 app.use(express.json());
 
 app.use('/api/auth',authRouter)
