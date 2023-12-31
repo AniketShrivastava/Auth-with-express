09c415183bbde3b8c81b58fa7f56b074ccb98f4e
Aniket shrivastava <aniket.shrivastava91@gmail.com>
2023-12-31 17:05:35 +0530

    add http cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const authRouter = require("./routes/authRoute");
 const databaseconnect = require("./config/db");
+const cors = require("cors")
 const cookieParser = require("cookie-parser")
 
 
@@ -8,7 +9,13 @@ require("dotenv").config();
 
 const app = express();
 
+
+
 databaseconnect();
+app.use(cors({
+    origin:[process.env.CLIENT_URL],
+    credentials:true
+}))
 app.use(cookieParser());
 
 app.use(express.json());
